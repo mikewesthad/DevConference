824c3e2741d05961eaf750d9b829dc754674b428
mikewesthad <michaelwesthadley@gmail.com>
2024-01-17 20:42:04 +0000

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <footer className={styles.footer}>
+        <a href="https://replit.com" target="_blank" rel="noopener noreferrer">
+          Powered by
+          <span className={styles.logo}>
+            <Image src="/replit.svg" alt="Replit Logo" width={20} height={18} />
+          </span>
+          Replit
+        </a>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
